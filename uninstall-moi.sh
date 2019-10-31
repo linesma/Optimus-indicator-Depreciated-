3239ee5e8ba137e79e14713f76d9c52387faf11c
Turysaz <turysaz@posteo.org>
2019-10-31 13:29:07 +0100

    Fix uninstall script syntax error

--- uninstall-moi.sh
+++ uninstall-moi.sh
@@ -14,7 +14,7 @@ rm -rf /usr/share/icons/hicolor/symbolic/apps/manjaroptimus-intel-symbolic.svg
 rm -rf /usr/share/icons/hicolor/symbolic/apps/manjaroptimus-nvidia-symbolic.svg
 rm -rf /usr/share/manjaroptimus-appindicator/scripts/pkexec_nvidia
 rm -rf /usr/share/manjaroptimus-appindicator/scripts/pkexec_intel
-rm- rf /usr/share/polkit-1/actions/org.freedesktop.policykit.set-nvidia.sh.policy
+rm -rf /usr/share/polkit-1/actions/org.freedesktop.policykit.set-nvidia.sh.policy
 rm -rf /usr/share/polkit-1/actions/org.freedesktop.policykit.set-intel.sh.policy
 rm -rf /etc/xdg/autostart/manjaroptimus-appindicator.desktop
 rm -rf /usr/bin/manjaroptimus-appindicator
