1ce86e90d7ef01e37ae7fdb8952e6b434c4540fe
M. Lines <21088368+linesma@users.noreply.github.com>
2019-11-01 20:47:27 +0700

    Update to version 2.0

--- setupgn.py
+++ setupgn.py
@@ -4,7 +4,7 @@ import os
 from distutils.core import setup
 
 setup(name="manjaroptimusindicator",
-      version="1.3.5",
+      version="2.0.0",
       description="Manjaro-Optimus Application Indicator",
       url='https://github.com/linesma/manjaroptimus-appindicator',
       author='Mark Lines',
@@ -12,7 +12,7 @@ setup(name="manjaroptimusindicator",
       packages=["manjaroptimusindicator"],
       data_files=[
           ('/usr/share/icons/hicolor/symbolic/apps/', ['icons/manjaroptimus-symbolic.svg', 'icons/manjaroptimus-intel-symbolic.svg', 'icons/manjaroptimus-nvidia-symbolic.svg']),
-          ('/usr/share/manjaroptimus-appindicator/scripts/', ['scripts/pkexec_nvidia', 'scripts/pkexec_intel']),
+          ('/usr/share/manjaroptimus-appindicator/scripts/', ['scripts/pkexec_nvidia', 'scripts/pkexec_intel', 'scripts/reboot']),
           ('/usr/local/bin/', ['gnome-script/set-intel.sh', 'gnome-script/set-nvidia.sh']), 
           ('/usr/share/polkit-1/actions/', ['pkexec/org.freedesktop.policykit.set-intel.sh.policy', 'pkexec/org.freedesktop.policykit.set-nvidia.sh.policy']),
           ('/etc/xdg/autostart/', ['autostart/manjaroptimus-appindicator.desktop'])],
@@ -23,6 +23,7 @@ os.chmod ('/etc/xdg/autostart/manjaroptimus-appindicator.desktop', 0o755)
 os.chmod ('/usr/bin/manjaroptimus-appindicator', 0o755)
 os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/pkexec_nvidia', 0o755)
 os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/pkexec_intel', 0o755)
+os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/reboot', 0o755)
 os.chmod ('/usr/local/bin/set-intel.sh', 0o755)
 os.chmod ('/usr/local/bin/set-nvidia.sh', 0o755)
 
