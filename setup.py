4f408c0de568d563b14d386a8945bc2a04d32749
M. Lines <21088368+linesma@users.noreply.github.com>
2023-03-06 14:30:11 +0700

    Reformated to meet new python standards

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name="manjaroptimusindicator",
-      version="3.0",
-      description="Manjaro-Optimus Application Indicator",
-      url='https://github.com/linesma/manjaroptimus-appindicator',
-      author='Mark Lines',
-      license='GPLv3',
-      packages=["manjaroptimusindicator"],
-      data_files=[
-          ('/usr/share/icons/hicolor/symbolic/apps/', ['icons/manjaroptimus-symbolic.svg', 'icons/manjaroptimus-intel-symbolic.svg', 'icons/manjaroptimus-nvidia-symbolic.svg']),
-          ('/usr/share/manjaroptimus-appindicator/scripts/', ['scripts/pkexec_nvidia', 'scripts/pkexec_intel', 'scripts/reboot.sh']),
-          ('/usr/share/polkit-1/actions/', ['pkexec/org.freedesktop.policykit.set-intel.sh.policy', 'pkexec/org.freedesktop.policykit.set-nvidia.sh.policy']),
-          ('/etc/xdg/autostart/', ['autostart/manjaroptimus-appindicator.desktop'])],
-      scripts=["bin/manjaroptimus-appindicator"]
+setup(
+    name="Optimus-indicator",
+    version="3.0",
+    description="Manjaro-Optimus Application Indicator",
+    url='https://github.com/linesma/manjaroptimus-appindicator',
+    author='Mark Lines',
+    license='GPLv3',
+    packages=["Optimus-indicator", "Optimus-indicator.manjaroptimusindicator", "Optimus-indicator.scripts", "Optimus-indicator.autostart", "Optimus-indicator.bin", "Optimus-indicator.icons", "Optimus-indicator.pkexec"],
+    include_package_data=True,
+    package_data={
+            "Optimus-indicator": [
+	      "manjaroptimusindicator/*",
+            "icons/*.svg",
+            "scripts/*",
+            "autostart/*.desktop",
+		"pkexec/*",
+            "bin/*"
+        ]
+    },
+    scripts=["bin/manjaroptimus-appindicator"],
 )
 
 os.chmod ('/etc/xdg/autostart/manjaroptimus-appindicator.desktop', 0o755)
@@ -25,3 +32,4 @@ os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/pkexec_intel', 0o755)
 os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/reboot.sh', 0o755)
 #os.chmod ('/usr/local/bin/set-intel.sh', 0o755)
 #os.chmod ('/usr/local/bin/set-nvidia.sh', 0o755)
+
