df46ac266600672d19fcbf8bb664957de377ffbf
M. Lines <21088368+linesma@users.noreply.github.com>
2023-03-07 00:51:28 +0700

    script to change permissions.

---
+++ post_install.py
@@ -0,0 +1,9 @@
+import os
+
+os.chmod ('/etc/xdg/autostart/manjaroptimus-appindicator.desktop', 0o755)
+os.chmod ('/usr/bin/manjaroptimus-appindicator', 0o755)
+os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/pkexec_nvidia', 0o755)
+os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/pkexec_intel', 0o755)
+os.chmod ('/usr/share/manjaroptimus-appindicator/scripts/reboot.sh', 0o755)
+#os.chmod ('/usr/local/bin/set-intel.sh', 0o755)
+#os.chmod ('/usr/local/bin/set-nvidia.sh', 0o755)
